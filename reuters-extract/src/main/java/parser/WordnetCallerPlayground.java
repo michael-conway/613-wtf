11bd53471cfe79475c2e48a1bd3ccb2bda353124
twmoore03 <M01109087t!>
2016-11-04 13:53:07 -0400

    added javaWordNetInterface from MIT

---
+++ reuters-extract/src/main/java/parser/WordnetCallerPlayground.java
@@ -0,0 +1,36 @@
+package parser;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import edu.mit.jwi.Dictionary;
+import edu.mit.jwi.IDictionary;
+import edu.mit.jwi.item.IIndexWord;
+import edu.mit.jwi.item.IWord;
+import edu.mit.jwi.item.IWordID;
+import edu.mit.jwi.item.POS;
+
+public class WordnetCallerPlayground {
+		
+		public static void main(String[] args) throws IOException {
+			testDictionary();
+		}
+	
+		public static void testDictionary() throws IOException {
+			// construct the URL to the Wordnet dictionary directory
+			String wnhome = System.getenv("WORDNET");
+			String path = wnhome + File.separator + "dict";
+			URL url = new URL("file", null, path);
+			
+			// construct the dictionary object and open it
+			IDictionary dict = new Dictionary(url);
+			dict.open();
+			
+			// look up first sense of the word "dog"
+			IIndexWord idxWord = dict.getIndexWord("dog", POS.NOUN);
+			IWordID wordID = idxWord.getWordIDs().get(0);
+			IWord word = dict.getWord(wordID);
+			System.out.println("Id = " + wordID);
+		}
+}
